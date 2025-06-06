caa9f0f8f60ad4434cccf55bc90a28869c3b1d82
FartyNaA2 <168141316+FartyNaA2@users.noreply.github.com>
2025-06-06 08:34:01 +0200

    API monobank 2.py

--- API monobank 2.py
+++ API monobank 2.py
@@ -1,5 +1,4 @@
-from fastapi import FastAPI, Query, HTTPException
-from typing import List
+from fastapi import FastAPI, HTTPException
 import requests
 
 app = FastAPI()
@@ -8,12 +7,12 @@ currency_map = {"USD": 840, "EUR": 978, "JPY": 392}
 
 @app.get("/")
 def root():
-    return {"message": "Сервіс працює! Щоб отримати курси валют, зверніться до /rates"}
+    return {
+        "message": "Сервіс працює! Дані про курси валют доступні на /rates"
+    }
 
 @app.get("/rates")
-def get_rates(
-    currencies: List[str] = Query(..., description="Валюти для перевірки")
-):
+def get_rates():
     response = requests.get("https://api.monobank.ua/bank/currency")
     if response.status_code != 200:
         raise HTTPException(status_code=500, detail="Monobank API error")
@@ -24,8 +23,9 @@ def get_rates(
     for item in all_data:
         code_a = item.get("currencyCodeA")
         code_b = item.get("currencyCodeB")
+
         for name, code in currency_map.items():
-            if code_a == code and code_b == 980 and name in currencies:
+            if code_a == code and code_b == 980:
                 result[name] = {
                     "rateBuy": item.get("rateBuy", "—"),
                     "rateSell": item.get("rateSell", "—")
